7684891f9793c6223f7c9d848fc7970a3ef6d3cb
AmrutaBagde <140888708+AmrutaBagde@users.noreply.github.com>
2023-08-04 16:02:20 +0530

    Add files via upload

---
+++ c/loop.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int h,r;
+printf("Enter any hour:");
+scanf("%d",&h);
+printf("Enter any rate:");
+scanf("%d",&r);
+printf("Daily wages of a worker:%d\n",r*h);
+}
+
